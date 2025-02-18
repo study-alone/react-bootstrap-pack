7d84713a4f200f2ab81eb4574d2e37348e78e9ee
mkchun <yikl1004@gmail.com>
2025-02-18 23:28:00 +0900

    dropdown 문석 작성 중

---
+++ apps/docs/src/app/restart-ui/API/dropdonw/DropdownDefault.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Dropdown } from '@repo/restart-ui'
+
+export const DropdownDefault = () => {
+	return (
+		<Dropdown placement="top-start">
+			<span>This is Toggle!</span>
+		</Dropdown>
+	)
+}
