7874c3a66124b9e412bef2245a733b53be2dcde8
mkchun <yikl1004@gmail.com>
2025-02-18 23:27:34 +0900

    props table 컴포넌트 추가

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,8 +1,40 @@
+// @ts-nocheck
 import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs'
 
-const docsComponents = getDocsMDXComponents()
+const { tr: Tr, th: Th, table: Table, img: NextraImage, ...docsComponents } = getDocsMDXComponents()
 
 export const useMDXComponents: typeof getDocsMDXComponents = (components) => ({
 	...docsComponents,
+	tr: Tr,
+	th: Th,
+	thead: ({ children, ...props }) => {
+		return (
+			<thead {...props}>
+				{children.props.children[0].props.children ? (
+					children
+				) : (
+					<Tr>
+						{['name', 'required', 'type', 'default value', 'description'].map(
+							(title) => (
+								<Th align="left">{title}</Th>
+							),
+						)}
+					</Tr>
+				)}
+			</thead>
+		)
+	},
+	tbody: (props) => (
+		<tbody
+			className="break-words first:[&_td]:font-semibold first:[&_td]:text-violet-600 first:[&_td]:dark:text-violet-500 [&_tr]:!bg-transparent"
+			{...props}
+		/>
+	),
+	table: (props) => <Table className="w-full text-sm" {...props} />,
+	img: (props) => (
+		<NextraImage {...props} className="nextra-border rounded-xl border drop-shadow-sm" />
+	),
+	figure: (props) => <figure className="mt-6" {...props} />,
+	figcaption: (props) => <figcaption className="mt-2 text-center text-sm" {...props} />,
 	...components,
 })
--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,5 +1,6 @@
-import type { NextConfig } from 'next'
+// import type { NextConfig } from 'next'
 import nextra from 'nextra'
+import remarkEmoji from 'remark-emoji'
 
 const withNextra = nextra({
 	latex: true,
@@ -7,14 +8,20 @@ const withNextra = nextra({
 		codeblocks: false,
 	},
 	// contentDirBasePath: '/docs',
+	mdxOptions: {
+		remarkPlugins: [remarkEmoji],
+	},
 })
 
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig: NextConfig = {
+const nextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	experimental: {
+		esmExternals: true,
+	},
 	output: 'export',
 	images: {
 		unoptimized: true,
